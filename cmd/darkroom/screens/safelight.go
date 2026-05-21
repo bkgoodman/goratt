a72cfa73a3381bfa37641842d286a951577ec687
Brad Goodman <brad@bradgoodman.com>
2026-05-21 19:34:36 +0000

    Safelight animate

--- cmd/darkroom/screens/safelight.go
+++ cmd/darkroom/screens/safelight.go
@@ -3,11 +3,15 @@
 package darkroomscreens
  
 import (
+	"time"
+
 	"goratt/lib/video/screen"
 )
  
 type SafeLightScreen struct {
-	mgr *screen.Manager
+	mgr        *screen.Manager
+	timerID    screen.TimerID
+	blinkState bool
 }
  
 func NewSafeLightScreen() *SafeLightScreen {
@@ -16,18 +20,39 @@ func NewSafeLightScreen() *SafeLightScreen {
  
 func (s *SafeLightScreen) Init(mgr *screen.Manager) {
 	s.mgr = mgr
+	s.blinkState = false
+	s.startTimer()
+}
+
+func (s *SafeLightScreen) startTimer() {
+	s.timerID = s.mgr.SetTimeout(500*time.Millisecond, func(scr screen.Screen) {
+		if s.timerID == 0 {
+			return
+		}
+		s.blinkState = !s.blinkState
+		s.Update()
+		s.startTimer()
+	})
 }
  
 func (s *SafeLightScreen) Update() {
-	s.mgr.FillBackground(1, 1, 0) // Yellow background
+	bgR, bgG, bgB := 1.0, 1.0, 0.0 // Yellow background
+	fgR, fgG, fgB := 1.0, 0.0, 0.0 // Red text
+
+	if s.blinkState {
+		// Invert colors to blink (Red background, Yellow text)
+		bgR, bgG, bgB = 1.0, 0.0, 0.0
+		fgR, fgG, fgB = 1.0, 1.0, 0.0
+	}
+
+	s.mgr.FillBackground(bgR, bgG, bgB)
  
 	s.mgr.SetFontSize(64)
 	y := float64(s.mgr.Height()/2) - 40
-	s.mgr.DC().SetRGB(1, 0, 0) // Red text
-	s.mgr.DrawCentered("SAFE-LIGHT ON", y, 1, 0, 0)
+	s.mgr.DrawCentered("SAFE-LIGHT ON", y, fgR, fgG, fgB)
  
 	s.mgr.SetFontSize(48)
-	s.mgr.DrawCentered("DO NOT ENTER", y+80, 1, 0, 0)
+	s.mgr.DrawCentered("DO NOT ENTER", y+80, fgR, fgG, fgB)
  
 	s.mgr.Flush()
 }
@@ -41,6 +66,7 @@ func (s *SafeLightScreen) HandleEvent(event screen.Event) bool {
 }
  
 func (s *SafeLightScreen) Exit() {
+	s.timerID = 0
 }
  
 func (s *SafeLightScreen) Name() string {
